87b1015a8d7d9ca4afcf40d6d89721ca1734f099
Ecstasy <jorge.puente@ecstasy.cl>
2018-03-28 12:22:00 -0300

    Modifico Menú y agrego imágenes para que observe shix

--- app/src/main/java/cl/ecstasy/marvelapp/MainActivity.kt
+++ app/src/main/java/cl/ecstasy/marvelapp/MainActivity.kt
@@ -2,11 +2,19 @@ package cl.ecstasy.marvelapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        bt_informacion.setOnClickListener({
+
+            Toast.makeText(this, "holiiii ", Toast.LENGTH_SHORT).show()
+
+        })
     }
 }
