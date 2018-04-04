5cc8ffd8d8bfc9bf47370ee726d997153de27fee
Shinox <jconcha.arev@gmail.com>
2018-04-04 08:47:56 -0300

    COMMIT packages 1

---
+++ app/src/main/java/cl/ecstasy/marvelapp/controller/Login.kt
@@ -0,0 +1,14 @@
+package cl.ecstasy.marvelapp.controller
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import cl.ecstasy.marvelapp.R
+
+class Login : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+        
+    }
+}
